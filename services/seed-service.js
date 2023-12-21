1c3ed078b607dd21b1b6cde71632c97ebd1182f9
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-12-21 15:14:45 -0500

    Fix: TypeError: bip32.fromSeed is not a function

--- services/seed-service.js
+++ services/seed-service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const bip39 = require('bip39');
-const bip32 = require('bip32');
+const bip32 = require('bip32').default();
 const { Keypair } = require('@solana/web3.js');
 const { derivePath } = require('ed25519-hd-key');
 
