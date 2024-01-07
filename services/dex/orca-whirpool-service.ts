7210dc3ee0025bd281dcdc898407b8677dc720cf
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-01-07 14:23:02 -0500

    Feat: Implemented Price Fetching

--- services/dex/orca-whirpool-service.ts
+++ services/dex/orca-whirpool-service.ts
@@ -1,7 +1,9 @@
 import {
   ExecutableRoute,
+  ORCA_SUPPORTED_TICK_SPACINGS,
   ORCA_WHIRLPOOLS_CONFIG,
   ORCA_WHIRLPOOL_PROGRAM_ID,
+  PriceModule,
   WhirlpoolContext,
   WhirlpoolData,
   buildWhirlpoolClient,
@@ -9,6 +11,7 @@ import {
 } from "@orca-so/whirlpools-sdk";
 import { BN, Address } from "@coral-xyz/anchor";
 import { PublicKey, Connection } from "@solana/web3.js";
+import Decimal from "decimal.js";
 
 
 let poolsAccounts: ReadonlyMap<string, WhirlpoolData> | null = null;
@@ -23,71 +26,57 @@ export async function getTokenPrice(
   connection: Connection,
   publicKey: PublicKey,
   tokenMint: Address
-): Promise<number | null> {
-  const wclient = buildWhirlpoolClient(
-    WhirlpoolContext.from(
-      connection,
-      {
-        publicKey: publicKey,
-        signAllTransactions: async () => {
-          return [];
-        },
-        signTransaction: async () => {
-          throw "error";
-        },
+): Promise<Decimal | null> {
+  const ctx = WhirlpoolContext.from(
+    connection,
+    {
+      publicKey: publicKey,
+      signAllTransactions: async () => {
+        return [];
       },
-      ORCA_WHIRLPOOL_PROGRAM_ID
-    )
+      signTransaction: async () => {
+        throw "error";
+      },
+    },
+    ORCA_WHIRLPOOL_PROGRAM_ID
   );
-
-  // Gets all the whirlpools from the default Whirlpool Config and Program
-  if (!poolsAccounts) {
+  const wclient = buildWhirlpoolClient(ctx);
+  
+  /* if (!poolsAccounts) {
     poolsAccounts = await getAllWhirlpoolAccountsForConfig({
       connection: connection,
       programId: ORCA_WHIRLPOOL_PROGRAM_ID,
-      configId: ORCA_WHIRLPOOLS_CONFIG,
+      configId: ORCA_WHIRLPOOLS_CONFIG
     });
-  }
+  } */
 
-  console.log("Found Whirlpool: " + JSON.stringify(Array.from(poolsAccounts.entries())[0]));
+  const USDC_MINT_KEY = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
+  const USDC_MINT = new PublicKey(USDC_MINT_KEY);
 
-  const pools = Array.from(poolsAccounts.entries()).map((poolAccount) => poolAccount[0]);
-
-  const router = await wclient.getRouter(pools);
-  let route: ExecutableRoute | null = null;
-  try {
-    route = await router.findBestRoute({
-      amountSpecifiedIsInput: true,
-      tokenIn: tokenMint, // Token from which is required the price
-      tokenOut: new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"), // Stablecoin Token on USD (USDC)
-      tradeAmount: new BN(1000000), // TODO: Get the amount of tokens that the user have
-    });
-  } catch (error) {
-    console.error("Error Finding Route");
-    error;
-    return null;
-  }
-
-  if (route) {
-    return route[0].totalAmountOut.toNumber();
-
-    // TODO: Get the decimals of the token
-    // TODO: Divide by decimals
-  } else {
-    return null;
-  }
+  const prices = await PriceModule.fetchTokenPricesByMints(ctx.fetcher, [
+    tokenMint,
+    USDC_MINT, //  USDC Stablecoin Token
+  ], {
+    quoteTokens: [
+      USDC_MINT,
+    ],
+    programId: ORCA_WHIRLPOOL_PROGRAM_ID,
+    whirlpoolsConfig: ORCA_WHIRLPOOLS_CONFIG,
+    tickSpacings: ORCA_SUPPORTED_TICK_SPACINGS,
+  });
+  return prices[tokenMint.toString()]
 }
 
 export async function getTokensPrice(
   connection,
   publicKey
-): Promise<number | null> {
+): Promise<Decimal | null> {
   // TODO: List all Associated Token Accounts
   // TODO: Query Each price to Orca Whirpool Service
 
   return await getTokenPrice(
     connection,
     publicKey,
-    new PublicKey("DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263") // BONK Token (For testing)
+    "DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263" // BONK Token (For testing)
   );
 }
