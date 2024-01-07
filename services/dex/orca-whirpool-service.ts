93f6c6932b59e96850ce824ad4aa018c675d2ee7
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-01-07 10:45:01 -0500

    Feat: Implements partially getTokensPrice

--- services/dex/orca-whirpool-service.ts
+++ services/dex/orca-whirpool-service.ts
@@ -8,7 +8,7 @@ import { PublicKey, Connection } from "@solana/web3.js";
  * @param {PublicKey} publicKey - The public key of the wallet account
  * @param {Address} tokenMint - The public key address of the token mint account of the requested token
  */
-export async function getTokenPrice(connection, publicKey, tokenMint) {
+export async function getTokenPrice(connection: Connection, publicKey: PublicKey, tokenMint: Address): Promise<number | undefined> {
   const wclient = whirpools.buildWhirlpoolClient(
     whirpools.WhirlpoolContext.from(
       connection,
@@ -26,19 +26,27 @@ export async function getTokenPrice(connection, publicKey, tokenMint) {
     .findBestRoute({
       amountSpecifiedIsInput: true,
       tokenIn: tokenMint, // Token from which is required the price
-      tokenOut: new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"), // Stablecoin Token on USD
-      tradeAmount: new BN(10), // TODO: Get the amount of tokens that the user have
+      tokenOut: new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"), // Stablecoin Token on USD (USDC)
+      tradeAmount: new BN(1000000), // TODO: Get the amount of tokens that the user have
     });
   
   if (route) {
-    route[0].totalAmountOut.toNumber();
+    return route[0].totalAmountOut.toNumber();
 
     // TODO: Get the decimals of the token
     // TODO: Divide by decimals
+  } else {
+    return undefined;
   }
 }
 
 export async function getTokensPrice(connection, publicKey) {
   // TODO: List all Associated Token Accounts
   // TODO: Query Each price to Orca Whirpool Service
+
+  return await getTokenPrice(
+    connection,
+    publicKey,
+    new PublicKey("DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263") // BONK Token (For testing)
+  );
 }
