6ca8d680dad02d7c2bf7253cdc785f2d5dee48ba
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-01-07 18:13:55 -0500

    Feat: Webpack TS Config

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.cjs', '.mjs']
   }
 };
