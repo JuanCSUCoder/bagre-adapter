a14c2f6af79e72281fde7bbb696076ab3e78b115
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-01-09 16:31:15 -0500

    Feat: Test Balance Calculation

--- tests/services/solana/solana-balance-service.test.ts
+++ tests/services/solana/solana-balance-service.test.ts
@@ -1,13 +1,21 @@
 import { test } from "@jest/globals"
-import { getPrices } from "../../../src/services/solana/solana-balance-service";
+import { getBalance, getPrices } from "../../../src/services/solana/solana-balance-service";
 import { Connection, Keypair, PublicKey } from "@solana/web3.js";
 
 test('It can retrieve prices from Orca Whirpools succesfully', async () => {
   const conn = new Connection(process.env.RPC_URL || "", "finalized")
   const keypair = Keypair.generate();
   
-  const price = await getPrices(conn, new PublicKey(process.env.TEST_PUBKEY || keypair.publicKey.toString()));
-  console.log(` Price in USDC: ${JSON.stringify(price)}`);
+  const prices = await getPrices(conn, new PublicKey(process.env.TEST_PUBKEY || keypair.publicKey.toString()));
+  console.log(` Price in USDC: ${JSON.stringify(prices)}`);
 
-  expect(price).not.toBeNull();
+  expect(prices).not.toBeNull();
 }, 30000)
+
+test('It can calculate the balance from Whirlpools data succesfully',async () => {
+  const conn = new Connection(process.env.RPC_URL || "", "finalized");
+  const keypair = Keypair.generate();
+
+  const balances = await getBalance(conn, new PublicKey(process.env.TEST_PUBKEY || keypair.publicKey.toString()));
+  console.log(`Balances in USDC: ${JSON.stringify(balances)}`);
+})
